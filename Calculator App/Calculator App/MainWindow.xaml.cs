a20231baa1d7a9147b61c6c6d840726a61b6df7c
TotaledToast <81196042+TotaledToast@users.noreply.github.com>
2022-03-24 09:51:55 +0000

    Update MainWindow.xaml.cs
    Added a single clear button, improved button pressing compactness

--- Calculator App/Calculator App/MainWindow.xaml.cs
+++ Calculator App/Calculator App/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,57 +25,18 @@ namespace Calculator_App
             InitializeComponent();
         }
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            AddToCalculationBox(Button1.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button1.Content;
+            AddToCalculationBox(((Button)sender).Content.ToString());
+            Stacks.CurrentNumber = Stacks.CurrentNumber + ((Button)sender).Content;
+        }
+        private void OperatorButton_Click(object sender, RoutedEventArgs e)
+        {
+            CollapseCurrentNumber();
+            Stacks.Operations.Add(((Button)sender).Content.ToString());
+            AddToCalculationBox(((Button)sender).Content.ToString());
         }
 
-        private void Button2_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button2.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button2.Content;
-        }
-        private void Button3_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button3.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button3.Content;
-        }
-        private void Button4_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button4.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button4.Content;
-        }
-        private void Button5_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button5.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button5.Content;
-        }
-        private void Button6_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button6.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button6.Content;
-        }
-        private void Button7_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button7.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button7.Content;
-        }
-        private void Button8_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button8.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button8.Content;
-        }
-        private void Button9_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button9.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button9.Content;
-        }
-        private void Button0_Click(object sender, RoutedEventArgs e)
-        {
-            AddToCalculationBox(Button0.Content.ToString());
-            Stacks.CurrentNumber = Stacks.CurrentNumber + Button0.Content;
-        }
 
         private void EqualsButtonClick(object sender, RoutedEventArgs e)
         {
@@ -85,38 +46,10 @@ namespace Calculator_App
             {
                 Output = Output + " " + x;
             }
-            Output = Output +" = "+ SolveInfix(Stacks.Operations).ToString();
+            Output = Output + " = " + SolveInfix(Stacks.Operations).ToString();
             OutputBox.Text = Output;
             ClearButton_Click(sender, e);
             ListClear();
-        }
-
-        private void DivideButton_Click(object sender, RoutedEventArgs e)
-        {
-            CollapseCurrentNumber();
-            Stacks.Operations.Add(DivideButton.Content.ToString());
-            AddToCalculationBox(DivideButton.Content.ToString());
-        }
-
-        private void MultiplyButton_Click(object sender, RoutedEventArgs e)
-        {
-            CollapseCurrentNumber();
-            Stacks.Operations.Add(MultiplyButton.Content.ToString());
-            AddToCalculationBox(MultiplyButton.Content.ToString());
-        }
-
-        private void MinusButton_Click(object sender, RoutedEventArgs e)
-        {
-            CollapseCurrentNumber();
-            Stacks.Operations.Add(MinusButton.Content.ToString());
-            AddToCalculationBox(MinusButton.Content.ToString());
-        }
-
-        private void PlusButton_Click(object sender, RoutedEventArgs e)
-        {
-            CollapseCurrentNumber();
-            Stacks.Operations.Add(PlusButton.Content.ToString());
-            AddToCalculationBox(PlusButton.Content.ToString());
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
@@ -124,6 +57,44 @@ namespace Calculator_App
             ListClear();
             Stacks.CurrentNumber = "";
             CalculationBox.Text = "";
+        }
+
+        private void ClearButtonSingle_Click(object sender, RoutedEventArgs e)
+        {
+            string RemoveSingleFromCurrentNumber()
+            {
+                string TempNum = "";
+                for (int i = 0; i < Stacks.CurrentNumber.Length - 1; i++)
+                {
+                    TempNum = TempNum + Stacks.CurrentNumber[i];
+                }
+                return TempNum;
+            }
+            if (Stacks.Operations.Count == 0)
+            {
+                if(Stacks.CurrentNumber != "")
+                {
+                    Stacks.CurrentNumber = RemoveSingleFromCurrentNumber();
+                }
+            }
+            else if(Stacks.CurrentNumber == "")
+            {
+                Stacks.Operations.RemoveAt(Stacks.Operations.Count - 1);
+            }
+            else
+            {
+                Stacks.CurrentNumber = RemoveSingleFromCurrentNumber();
+            }
+            CalculationBox.Text = "";
+            foreach (string x in Stacks.Operations)
+            {
+                CalculationBox.Text = CalculationBox.Text + x;
+            }
+            if (Stacks.CurrentNumber != "")
+            {
+                CalculationBox.Text = CalculationBox.Text + Stacks.CurrentNumber;
+            }
+
         }
         private void AddToCalculationBox(string Input)
         {
@@ -137,7 +108,7 @@ namespace Calculator_App
                 Stacks.CurrentNumber = "";
             }
         }
-        private int SolveInfix(List<string> Infix)
+        private double SolveInfix(List<string> Infix)
         {
             Stacks.ToPostFix(Infix);
             return Stacks.SolvePostFix(Stacks.PostFix);
@@ -198,7 +169,7 @@ namespace Calculator_App
                 Stacks.PostFix.Add(x);
             }
         }
-        public static int SolvePostFix(List<string> Input)
+        public static double SolvePostFix(List<string> Input)
         {
             List<string> OperatorsList = new List<string> {
                 "/", "*", "-", "+"
@@ -227,7 +198,7 @@ namespace Calculator_App
                     i = i - 2;
                 }
             }
-            return Convert.ToInt32(Input[0]);
+            return Convert.ToDouble(Input[0]);
         }
     }
 }
